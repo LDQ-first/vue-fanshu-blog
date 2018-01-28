8bca840b805e065e92fd632e58927bcf5543d43f
LDQ-first <178848890@qq.com>
2018-01-28 15:57:48 +0800

    修复导航高亮问题

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="container">
-    <el-menu :router="true" :default-active="active" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+    <el-menu
+      ref='headerElMenu'
+      :router="true"
+      :default-active="active"
+      class="el-menu-demo"
+      mode="horizontal"
+      active-text-color="#409EFF"
+      @select="handleSelect">
       <el-menu-item index="/">主页</el-menu-item>
       <el-menu-item index="/list"><i class="fa fa-flag" aria-hidden="true"></i>探索</el-menu-item>
       <el-menu-item index="/signUp" class="right"><i class="fa fa-user-o" aria-hidden="true"></i>注册</el-menu-item>
@@ -15,12 +22,28 @@ export default {
   name: 'Header',
   data () {
     return {
-      active: '0'
+      active: '/'
     }
+  },
+  created () {
+    this.active = this.$route.path // 解决刷新不高亮
+    this.$router.afterEach((to, from) => {
+      this.active = to.path // 解决编程式切换路由不高亮
+    })
   },
   methods: {
     handleSelect (key, keyPath) {
       console.log(key, keyPath)
+    }
+  },
+  watch: {
+    '$route' (newVal, oldVal) {
+      console.log('$route: ', newVal)
+      if (newVal.path === '/') {
+        this.$refs.headerElMenu.$el.classList.add('firstActive')
+      } else {
+        this.$refs.headerElMenu.$el.classList.remove('firstActive')
+      }
     }
   }
 }
@@ -43,14 +66,23 @@ export default {
   .right {
     float: right;
   }
-  .el-menu-item:first-child {
-    margin-left: 0;
-    font-size: 25px;
-    font-weight: 100;
-    background: $baseColor;
-    color: #FFF;
-    &:hover {
+  .el-menu {
+    &.firstActive {
+       .el-menu-item:first-child {
+         box-shadow: 0 0 3px $baseColor;
+      }
+    }
+  }
+  .el-menu-item {
+    .fa {
+      margin-right: 0.5rem;
+    }
+    &:first-child {
+      margin-left: 0;
+      font-size: 25px;
+      font-weight: 100;
       background: $baseColor;
+      color: #FFF !important;
     }
   }
 }
